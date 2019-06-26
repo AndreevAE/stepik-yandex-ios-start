3e4568914f3d0bd27d1729c7662c30d3abf13611
Alexander Andreev <al101an@mail.ru>
2019-06-26 23:53:29 +0700

    Create Project Notes

---
+++ Notes/Notes/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Notes
+//
+//  Created by Александр Андреев on 26/06/2019.
+//  Copyright © 2019 Alexander Andreev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
