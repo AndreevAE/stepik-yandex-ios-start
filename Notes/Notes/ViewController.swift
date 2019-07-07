6b94424b1ddb8f64083aad36d9aa202ba503318d
Alexander Andreev <al101an@mail.ru>
2019-07-07 22:31:54 +0700

    Step 3.9. AutoLayout Edit Note Screen

--- Notes/Notes/ViewController.swift
+++ Notes/Notes/ViewController.swift
@@ -11,10 +11,25 @@ import CocoaLumberjack
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var noteTitleTextField: UITextField!
+    @IBOutlet weak var noteTextView: UITextView!
+    @IBOutlet weak var datePickerSwitcher: UISwitch!
+    @IBOutlet weak var datePicker: UIDatePicker!
+    @IBOutlet weak var selectedColorView: UIView!
+    @IBOutlet weak var secondColorView: UIView!
+    @IBOutlet weak var thirdColorView: UIView!
+    @IBOutlet weak var customColorView: UIView!
+    
+    @IBAction func switchDatePickerView(_ sender: UISwitch) {
+        self.datePicker.isHidden = !sender.isOn
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // Do any additional setup after loading the view.
+        self.setupView()
         DDLogInfo("ViewController didLoad")
     }
 
@@ -25,3 +40,10 @@ class ViewController: UIViewController {
 
 }
 
+private extension ViewController {
+    
+    func setupView() {
+        self.datePickerSwitcher.isOn = false
+        self.datePicker.isHidden = true
+    }
+}
